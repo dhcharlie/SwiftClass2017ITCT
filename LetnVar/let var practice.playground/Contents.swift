6afe44c413f1cac4502993c7878749b704e1df7a
Hakyung Noh <hakyung1992@gmail.com>
2017-09-19 10:12:31 +0900

    commite file
    commute practice

---
+++ LetnVar/let var practice.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//1. VAR
+
+var variableString = "Apple"
+//수정이 가능한 값 (Mutable)
+
+variableString += " and Mango"
+//Compile 에러 나지 않음
+
+
+
+//2. LET
+
+let constantString = "Apple"
+//수정이 불가능한 값 (Immutable)
+
+constantString += " and Banana"
+//Compile 에러 발생 (Change 'let' to 'var' to make it mutable)
+
+
+
+
